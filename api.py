4196fd1351423537816b53e89824fd2f23d203ba
admar317 <55062406+admar317@users.noreply.github.com>
2020-02-19 16:33:46 -0800

    Fixed createDatabaseHelper.py
    Note: requires linux/unix based file system to work.
    Currently returns names of files at specified path, should os.walk fail returns empty list.
    
    Previously:
    creating "createDatabaseHelper" function (work in progress)
    
    Eventually should be in a seperate branch. Note: Currently not itterating through the for loop using os.walk. Perhaps because being used on Windows OS.

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 """
 By Samir and Adrian Marin Estrada
 """
-
+from createDatabaseHelper import list_of_files
 from flask import Flask
 from flask_restful import Resource, Api
 
@@ -12,6 +12,15 @@ class Dedomena(Resource):
     def get(self):
         return {'hello': 'dedomena'}
 
+# path_name is a string
+# gets list of files
+class CreateDatabase(Resource):
+	def get(self, path_name):
+		# print(path_name)
+		return list_of_files(path_name)
+		# return "Hello!"
+
+api.add_resource(CreateDatabase,'/<string:path_name>')
 api.add_resource(Dedomena, '/')
 
 if __name__ == '__main__':
